2647889796621a0e72b8f5305e8cfbb592ed9f02
Linda Thomson <linducya@gmail.com>
2025-02-26 12:37:31 +0000

    Added comments to run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-# Access and execute functions from routes.py
+# Import home() function from routes.py and print the return value
 from app.routes import home
+
 print(home())
 
+# Import and run main() from cli.py
 from app.cli import main
 
+# Run main() when the script is executed directly
 if __name__ == '__main__':
     main()
